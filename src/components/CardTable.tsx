5f7b38dc08f68f048cb1b6cb5a00d1dd19e62c78
developer-sahadat <mdridoysikder862@gmail.com>
2023-08-01 11:22:16 +0600

    update

--- src/components/CardTable.tsx
+++ src/components/CardTable.tsx
@@ -3,38 +3,75 @@ import React, { Fragment } from "react";
 const CardTable = () => {
   return (
     <Fragment>
-      <div className="overflow-x-auto ">
-        <table className="table">
-          {/* head */}
+      <div className="overflow-x-auto">
+        <table className="table table-xs table-pin-rows table-pin-cols">
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <td>Title</td>
+              <td>Author</td>
+              <td>Genre</td>
+              <td>Publication Date</td>
+
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr className="bg-base-200">
+            <tr>
               <th>1</th>
               <td>Cy Ganderton</td>
               <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            {/* row 2 */}
+              <td>Littel, Schaden and Vandervort</td>
+              <td>Canada</td>
+              </tr>
             <tr>
               <th>2</th>
               <td>Hart Hagerty</td>
               <td>Desktop Support Technician</td>
-              <td>Purple</td>
+              <td>Zemlak, Daniel and Leannon</td>
+              <td>United States</td>
             </tr>
-            {/* row 3 */}
             <tr>
               <th>3</th>
               <td>Brice Swyre</td>
               <td>Tax Accountant</td>
-              <td>Red</td>
+              <td>Carroll Group</td>
+              <td>China</td>
+            </tr>
+            <tr>
+              <th>4</th>
+              <td>Marjy Ferencz</td>
+              <td>Office Assistant I</td>
+              <td>Rowe-Schoen</td>
+              <td>Russia</td>
+            </tr>
+            <tr>
+              <th>5</th>
+              <td>Yancy Tear</td>
+              <td>Community Outreach Specialist</td>
+              <td>Wyman-Ledner</td>
+              <td>Brazil</td>
+            </tr>
+            <tr>
+              <th>6</th>
+              <td>Irma Vasilik</td>
+              <td>Editor</td>
+              <td>Wiza, Bins and Emard</td>
+              <td>Venezuela</td>
+            </tr>
+            <tr>
+              <th>7</th>
+              <td>Meghann Durtnal</td>
+              <td>Staff Accountant IV</td>
+              <td>Schuster-Schimmel</td>
+              <td>Philippines</td>
+            </tr>
+            <tr>
+              <th>8</th>
+              <td>Sammy Seston</td>
+              <td>Accountant I</td>
+              <td>O'Hara, Welch and Keebler</td>
+              <td>Indonesia</td>
             </tr>
           </tbody>
         </table>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,8 @@ import CardTable from "../components/CardTable";
 const Home = () => {
   return (
     <Fragment>
-      <section className="max-w-[1200px] mx-auto min-h-[75vh]">
+      <section className="max-w-[1200px] mx-auto min-h-[75vh] mt-6">
+        <h2 className="text-center text-2xl font-bold mb-3">New Collections</h2>
         <CardTable />
       </section>
     </Fragment>
